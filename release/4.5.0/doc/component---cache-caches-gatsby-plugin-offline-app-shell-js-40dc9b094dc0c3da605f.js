efdb03f5d51bd8a8a3b6413074136a8df5ddeb59
Dawid Weiss <dawid.weiss@carrotsearch.com>
2022-11-03 08:31:29 +0100

    Documentation for version 4.5.0

---
+++ release/4.5.0/doc/component---cache-caches-gatsby-plugin-offline-app-shell-js-40dc9b094dc0c3da605f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_apidocs=self.webpackChunkgatsby_starter_apidocs||[]).push([[306],{1367:function(t,e,a){var n=a(5318);e.__esModule=!0,e.default=void 0;var u=n(a(5354)),r=n(a(7294)),s=function(t){function e(){return t.apply(this,arguments)||this}return(0,u.default)(e,t),e.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},e}(r.default.Component),l=s;e.default=l}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-40dc9b094dc0c3da605f.js.map
